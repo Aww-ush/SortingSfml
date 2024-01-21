54ac7166b47f8ce8308b266612fa3782ede29331
Aayush554 <aayushshrestha554@gmail.com>
2024-01-21 14:56:57 -0500

    finished

--- main.cpp
+++ main.cpp
@@ -1,53 +1,87 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
-#include <vector> // Include the vector header
-
+#include <vector>
+#include <algorithm> // For shuffle
+#include <random>
+using namespace sf;
 using namespace std;
 
-const float THICKNESS = 50.f;
-
-void FillVector(vector<float> &vec) // Pass vector by reference
+class Field
 {
-    // fill the vector with random numbers
-    for (int i = 0; i < 20; i++)
-    {
-        vec.push_back(rand() % 500 + 100);
-    }
-}
+private:
+    vector<int> m_points;
 
-// Pass window by reference and vector by reference
-void DrawRectangles(const vector<float> &vec, sf::RenderWindow &window)
-{
-    for (int i = 0; i < vec.size(); i++)
+public:
+    void fill(const unsigned number_of_points)
     {
-        sf::RectangleShape rectangle(sf::Vector2f(THICKNESS, vec[i]));
-        rectangle.setPosition(50.f + i * (THICKNESS + 10.f), -50.f); // Adjust position
-        window.draw(rectangle);
+        // Creating the points
+        for (unsigned i = 0; i < number_of_points; ++i)
+        {
+            m_points.push_back(i+50);
+        }
+        // Shuffling it
+        std::random_device rd;
+        std::mt19937 g(rd());
+
+        // Shuffle the elements randomly
+        std::shuffle(m_points.begin(), m_points.end(), g);
     }
-}
+
+    // bubble sort
+    void BubbleSort()
+    {
+        for (size_t i = m_points.size() - 1; i > 0; --i)
+        {
+            bool swapped = false;
+            for (size_t pos = 0; pos < i; ++pos)
+            {
+                if (m_points[pos] < m_points[pos + 1])
+                {
+                    swap(m_points[pos], m_points[pos + 1]);
+                    swapped = true;
+                }
+            }
+            if (swapped)
+                break;
+        }
+    }
+
+    void show_points(RenderWindow &App)
+    {
+        RectangleShape rectangle(Vector2f(1, 178));
+        rectangle.setFillColor(Color(129, 154, 120));
+        for (size_t i = 0; i < m_points.size(); i++)
+        {
+            rectangle.setPosition(i, m_points[i]);
+            App.draw(rectangle);
+        }
+    }
+};
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(1000, 1000), "My window");
-    vector<float> vec;
-    FillVector(vec);
+    RenderWindow App(VideoMode(800, 600), "Bubble Sort");
+    Event ev;
+    Field fieldContext;
+    fieldContext.fill(800);
 
-    window.setVerticalSyncEnabled(true);
-
-    while (window.isOpen())
+    // Let's start the app
+    while (App.isOpen())
     {
-        sf::Event event;
-        while (window.pollEvent(event))
+        // Verifying events
+        while (App.pollEvent(ev))
         {
-            if (event.type == sf::Event::Closed)
-                window.close();
+            // Window closed
+            if (ev.type == Event::Closed)
+                App.close();
         }
+        fieldContext.BubbleSort();
+        // Clearing screen
+        App.clear(Color(255, 247, 244));
+        fieldContext.show_points(App);
 
-        window.clear(sf::Color::Black);
-        DrawRectangles(vec, window);
-
-        window.display();
+        // Displaying
+        App.display();
     }
-
     return 0;
 }
